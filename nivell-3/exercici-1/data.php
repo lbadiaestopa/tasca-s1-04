12e7031efd1ea81ba2af69edd9cdad516fe62c82
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-24 17:50:20 +0100

    feat: add new data at mock cinema dataset

--- nivell-3/exercici-1/data.php
+++ nivell-3/exercici-1/data.php
@@ -9,5 +9,14 @@ $cinemas = [
         new Movie("The Matrix", 136, "Lana Wachowski, Lilly Wachowski"),
         new Movie("Interstellar", 169, "Christopher Nolan")
     ]),
-
+    new Cinema("Yelmo Cines", "Madrid", [
+        new Movie("The Dark Knight", 152, "Christopher Nolan"),
+        new Movie("Pulp Fiction", 154, "Quentin Tarantino"),
+        new Movie("The Grand Budapest Hotel", 99, "Wes Anderson")
+    ]),
+    new Cinema("Cinesa", "Valencia", [
+        new Movie("The Lord of the Rings: The Fellowship of the Ring", 178, "Peter Jackson"),
+        new Movie("The Lord of the Rings: The Two Towers", 179, "Peter Jackson"),
+        new Movie("The Lord of the Rings: The Return of the King", 201, "Peter Jackson")
+    ])
 ];
